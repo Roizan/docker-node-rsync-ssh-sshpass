5ee6c8eb3eaa364cd51a4453fd5f9d75be0b5ff0
Bernhard Meder <sinqous@googlemail.com>
2023-12-13 15:38:40 +0100

    Update build.sh
    removed overhead

--- build.sh
+++ build.sh
@@ -4,19 +4,7 @@ echo "Starting Docker buildx instance for multiplatform build"
 docker buildx create --use
 
 echo "Building Containers"
-docker buildx build --platform linux/amd64,linux/arm64 -t docker-node-rsync-ssh-sshpass14 - < Dockerfile14
-docker buildx build --platform linux/amd64,linux/arm64 -t docker-node-rsync-ssh-sshpass16 - < Dockerfile16
-docker buildx build --platform linux/amd64,linux/arm64 -t docker-node-rsync-ssh-sshpass17 - < Dockerfile17
-docker buildx build --platform linux/amd64,linux/arm64 -t docker-node-rsync-ssh-sshpass-latest - < Dockerfile
-
-echo "Tagging Containers"
-docker tag docker-node-rsync-ssh-sshpass14:latest bernhardmeder/docker-node-rsync-ssh-sshpass:node14-latest
-docker tag docker-node-rsync-ssh-sshpass16:latest bernhardmeder/docker-node-rsync-ssh-sshpass:node16-latest
-docker tag docker-node-rsync-ssh-sshpass17:latest bernhardmeder/docker-node-rsync-ssh-sshpass:node17-latest
-docker tag docker-node-rsync-ssh-sshpass:latest bernhardmeder/docker-node-rsync-ssh-sshpass:latest
-
-echo "Pushing to Dockerhub"
-docker push bernhardmeder/docker-node-rsync-ssh-sshpass:node14-latest
-docker push bernhardmeder/docker-node-rsync-ssh-sshpass:node16-latest
-docker push bernhardmeder/docker-node-rsync-ssh-sshpass:node17-latest
-docker push bernhardmeder/docker-node-rsync-ssh-sshpass:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t bernhardmeder/docker-node-rsync-ssh-sshpass:node14-latest -f Dockerfile14 --push . 
+docker buildx build --platform linux/amd64,linux/arm64 -t bernhardmeder/docker-node-rsync-ssh-sshpass:node16-latest -f Dockerfile16 --push . 
+docker buildx build --platform linux/amd64,linux/arm64 -t bernhardmeder/docker-node-rsync-ssh-sshpass:node17-latest -f Dockerfile17 --push . 
+docker buildx build --platform linux/amd64,linux/arm64 -t bernhardmeder/docker-node-rsync-ssh-sshpass:latest --push . 
